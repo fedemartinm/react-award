2feb818968b4ddb818c01e31cbed3d72df104f8f
Fede Martin <fedemartin94@gmail.com>
2023-01-09 19:00:30 -0300

    add basic example app

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,52 @@
 import 'react-app-polyfill/ie11';
+import '../dist/react-award.css';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import award from './images/award.svg';
+import dog from './images/dog.svg';
+
+import { Award } from '../.';
+
+import * as confetti from './animations/confetti.json';
+import * as lights from './animations/lights.json';
 
 const App = () => {
+  const [play, setPlay] = React.useState(false);
+
   return (
-    <div>
-      <Thing />
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      <div>
+        <Award play={play} image={award} animation={confetti} />
+        <button
+          style={{ width: '100%' }}
+          onClick={() => {
+            setPlay(true);
+          }}
+        >
+          Click me
+        </button>
+      </div>
+      <div>
+        <Award
+          image={dog}
+          style={{
+            height: 100,
+            width: 100,
+          }}
+          playOnHover={true}
+          animation={lights}
+        />
+        <p style={{ textAlign: 'center' }}>Play on hover</p>
+      </div>
     </div>
   );
 };
